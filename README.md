936597d34db66f63b55453c0d287630271927e4f
HormCodes <horm@horm.codes>
2023-01-13 18:04:04 +0100

    feat: add simple business card

---
+++ README.md
@@ -0,0 +1,18 @@
+
+# npx hormcodes
+
+A simple personal [Horm Codes](https://horm.codes/) business card to display in a terminal. 
+
+## Requirements and Usage
+
+Make sure you have [Node](https://nodejs.org/en/) and [npx](https://www.npmjs.com/package/npx) installed, and simply run:  
+
+```shell
+npx hormcodes
+```
+
+## Credits and License
+
+The project was inspired by [Atila Fassina](https://github.com/atilafassina) and his [npx-atila](https://github.com/atilafassina/npx-atila). The project is licensed under [MIT license](./LICENSE). 
+
+
---
+++ build.js
@@ -0,0 +1,34 @@
+const boxen = require("boxen");
+const fs = require("fs");
+const path = require("path");
+
+const handle = "HormCodes";
+
+const text = `Matěj 'Horm' Horák / @${handle}
+Backend Developer at Crystallize
+
+https://horm.codes
+
+twitter.com/${handle}
+github.com/${handle}
+linkedin.com/in/${handle}`;
+
+const output = boxen(text, {
+  padding: 1,
+  margin: 1,
+  borderStyle: "round",
+  borderColor: "#F26C4F",
+});
+
+const cli = `#!/usr/bin/env node
+console.log(\`${output}\`);`;
+
+const outputDirectoryName = "bin";
+const buildOutputDirectory = path.join(__dirname, outputDirectoryName);
+
+if (fs.existsSync(buildOutputDirectory)) {
+  fs.rmSync(buildOutputDirectory, { recursive: true });
+}
+fs.mkdirSync(buildOutputDirectory);
+
+fs.writeFileSync(path.join(__dirname, outputDirectoryName + "/index.js"), cli);
